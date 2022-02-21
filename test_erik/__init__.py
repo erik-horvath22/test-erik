14ba3bd54ff62495c11de3260a3308f4ffa0f0d3
Erik Horvath <erik.horvath@globant.com>
2022-02-21 14:32:39 +0200

    sSA

--- test_erik/__init__.py
+++ test_erik/__init__.py
@@ -1,4 +1,5 @@
 from .module1 import hello as hello1
 from .module2 import hello as hello2
+from .helpers.module3 import hello as hello3
 def hello(target="World"):
     print("Hello {}!".format(target))
