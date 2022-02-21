3a452e327ea92f4cff5d8bc1caf56f26c71c886d
Erik Horvath <erik.horvath@globant.com>
2022-02-21 10:14:11 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="test",
+    name="test-erik",
     version="1.0.0",
-    packages=find_packages('test')
+    packages=find_packages('test-erik')
 )
